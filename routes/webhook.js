64d80c12ea5d3c27973e89513635cd0157f9fab5
dhdc1 <dhdctraining@gmail.com>
2019-05-29 12:38:13 +0700

    tehnn

--- routes/webhook.js
+++ routes/webhook.js
@@ -2,7 +2,7 @@ var express = require('express');
 const request = require('request-promise')
 var router = express.Router();
 var token = "IEl91F7L1MnAuHDD3kxAYgF27xXSQJW41LpAoLMf0RjuuXNBjuN5E2uhYRHPyqcUKjRSgdwWconYrYivZLmyk/ECjXV+pNwjgQQoji+ZNs1wtUwwzkz3xMOjqbabRexZUCt2vbadhK7UwZkxBs9R+gdB04t89/1O/w1cDnyilFU="
-var dialogflow = "https://bots.dialogflow.com/line/81523972-407b-4542-9d91-539f4cc95baa/webhook"
+var dialogflow_api = "https://bots.dialogflow.com/line/81523972-407b-4542-9d91-539f4cc95baa/webhook"
 
 const line_reply = (req,messages) => {
     return request({
@@ -21,33 +21,19 @@ const line_reply = (req,messages) => {
 
 const postToDialogflow = req => {
     req.headers.host = "bots.dialogflow.com";
-    let r = request({
+    return request({
         method: "POST",
-        uri: dialogflow,
+        uri: dialogflow_api,
         headers: req.headers,
         body: JSON.stringify(req.body)
     });
-    console.log('df',JSON.stringify(r))
+    
 };
 
-router.post('/', function (req, res, next) {
-    console.log('webhook', JSON.stringify(req.headers))
+router.post('/', function (req, res, next) {   
     event = req.body.events[0];
-    if (event.message.type == "text") {
-        if (event.message.text == 'จองนัด') {
-            postToDialogflow(req)
-        } else {
-            line_reply(req,[
-                {
-                    'type':'text',
-                    'text':'ok...1'
-                },
-                {
-                    'type':'text',
-                    'text':'ok...2'
-                }
-            ])
-        }
+    if (event.message.type == "text") {       
+        console.log(postToDialogflow(req))      
     }
     return res.status(200).send(req.method);
 
